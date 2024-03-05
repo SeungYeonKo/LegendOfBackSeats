fa4569e2451e322a7284b8e893e11ff5bd1508fe
SeungYeonKo <createNewScene479@gmail.com>
2024-03-05 18:04:18 +0900

    [고승연]폴더생성

--- Assets/02.Scripts/Monster/MonsterAttack.cs
+++ Assets/02.Scripts/Monster/MonsterAttack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterMoveAbility : MonoBehaviour
+public class MonsterAttack : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
