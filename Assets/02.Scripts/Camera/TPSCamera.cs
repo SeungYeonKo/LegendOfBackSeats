0537b7ddb0cb341cc84e97efb14ef90e29bd2ff7
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-03-05 18:10:25 +0900

    [정수빈] 집가기 전 커밋

---
+++ Assets/02.Scripts/Camera/TPSCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 3인칭 슈팅 (Third Person Shooter) 
+
+public class TPSCamera : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
