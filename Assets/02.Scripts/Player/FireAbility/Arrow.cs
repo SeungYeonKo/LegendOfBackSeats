ced761ed485ef1475b91b34e4b07a4303fccf6d8
MirDragon40 <mirjeong6463@gmail.com>
2024-03-07 12:36:09 +0900

    [정수빈] 화살 스크립트 제작중

--- Assets/02.Scripts/Player/FireAbility/Arrow.cs
+++ Assets/02.Scripts/Player/FireAbility/Arrow.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 화살을 쏘면 포물선 형태로 날아가도록 하고싶다.
+
+
+    private Rigidbody arrowRigidbody;
+
+
+    [Header("화살의 강도")]
+    public float ArrowPower = 12f;
+
+
+
     void Start()
     {
-        
+        arrowRigidbody = GetComponent<Rigidbody>();
+        arrowRigidbody.AddForce(ArrowPower * transform.forward, ForceMode.Impulse);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() // 물리 연산은 FixedUpdate에서 처리하는 것이 좋음
     {
-        
+        if (arrowRigidbody.velocity.magnitude > 0.1f) // 속도가 충분히 클 때만 방향을 바꾸도록 
+        {
+            // 속도 벡터 방향으로 보도록 회전
+            Quaternion direction = Quaternion.LookRotation(arrowRigidbody.velocity.normalized);
+            arrowRigidbody.rotation = Quaternion.Slerp(arrowRigidbody.rotation, direction, Time.fixedDeltaTime * 20f);
+        }
+
     }
+
 }
--- Assets/02.Scripts/Player/FireAbility/ArrowFireAbility.cs
+++ Assets/02.Scripts/Player/FireAbility/ArrowFireAbility.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ArrowFireAbility : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject ArrowPrefab; // 발사할 화살 객체
+    public Transform ArrowContainer; // 화살이 위치할 곳
+
+    public float shotInterval = 2f; // 화살을 발사하는 간격
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)) // 왼쪽 마우스 버튼 클릭 시
+        {
+            // 화살 발사각도 = 20 + 20i (20, 40, 60)
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject tempObject = Instantiate(ArrowPrefab, ArrowContainer);
+                Vector3 direction = new Vector3(Mathf.Cos((20 + 20 * i) * Mathf.Deg2Rad), 0, Mathf.Sin((20 + 20 * i) * Mathf.Deg2Rad));
+                tempObject.transform.forward = direction; // 화살의 방향을 설정합니다.
+                tempObject.transform.position = transform.position + shotInterval * direction;
+                // shotInterval * direction은 화살이 플레이어 앞에서 발사되도록 자연스럽게 만들어주기 위해
+                // direction을 곱해 발사 각도에 맞게 발사 위치를 띄웁니다. 이는 화살이 나아가고자 하는 방향으로 간격을 띄우는 것입니다.
+            }
+        }
     }
+
 }
