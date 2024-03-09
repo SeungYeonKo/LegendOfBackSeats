d73371e925ac5960297eade39d5f976e4926e8eb
sjoonkim705 <sjoonkim705@gmail.com>
2024-03-09 15:34:14 +0900

    [김성준] 체력데이터, 체력UI, Die애니메이션 추가

---
+++ Assets/02.Scripts/UI/HealthUI.cs
@@ -0,0 +1,43 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    // todo: 플레이어 체력 데이터 불러와서 체력 변수 1당 하트 1개 생성
+    public ThirdPersonController Playerinfo;
+    private int _currentHealth;
+    private int _maxHealth;
+    private Image _heartImage;
+
+    private void Awake()
+    {
+        Playerinfo = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonController>();
+        _heartImage = GetComponentInChildren<Image>();
+    }
+    void Start()
+    {
+        _maxHealth = Playerinfo.MaxHealth; // initialize
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Playerinfo.CurrentHealth--;
+            Debug.Log(Playerinfo.CurrentHealth);
+        }
+        RefreshUI();
+
+    }
+    void RefreshUI()
+    {
+        _currentHealth = Playerinfo.CurrentHealth;
+        _heartImage.fillAmount = (float)_currentHealth / _maxHealth;
+
+    }
+}
