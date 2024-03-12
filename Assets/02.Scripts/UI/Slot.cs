ada2b00f18681ffa9047c394612890bc1dbe22db
SeungYeonKo <createNewScene479@gmail.com>
2024-03-12 11:44:56 +0900

    [고승연] 인벤 프리팹 수정후 다시 업로드

--- Assets/02.Scripts/UI/Slot.cs
+++ Assets/02.Scripts/UI/Slot.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Item Item;
+    public int ItemCount;
+    public Image ItemImage;
+
+    [SerializeField]
+    private Text TextCount;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
