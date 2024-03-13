25bb588844771f070b0140c8931be6ffdbfce5b2
MirDragon40 <mirjeong6463@gmail.com>
2024-03-13 15:29:15 +0900

    [정수빈] 중간커밋

--- Assets/02.Scripts/Scene/LobbyScene.cs
+++ Assets/02.Scripts/Scene/LobbyScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LobbyScene : MonoBehaviour
 {
@@ -21,6 +22,26 @@ public class LobbyScene : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnGameStartButton()
+    {
+        SceneManager.LoadScene((int)SceneNames.Loading);
+    }
+    public void OnTutorialButton()
+    {
+
+    }
+    public void OnGameExitButton()
+    {
+        Debug.Log("게임 종료 버튼 클릭");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        // 유니티 에디터 에서 실행했을 경우 종료하는 방법
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
     }
 
 }
