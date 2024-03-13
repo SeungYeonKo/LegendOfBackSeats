f3cbfb7ae4e6b6eb1b36a2cc58251ae78df80967
SeungYeonKo <createNewScene479@gmail.com>
2024-03-13 10:13:49 +0900

    [고승연] 플레이어 아이템 먹어지게 수정

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -32,7 +32,7 @@ public class Item
 
         if (ItemType == ItemType.Health)
         {
-            // Health아이템 사용시 플레이어 체력 꽉차기
+            // Health아이템 사용시 플레이어 체력 +5
             ThirdPersonController ThirdPersonController = GameObject.FindWithTag("Player").GetComponent<ThirdPersonController>();
             ThirdPersonController.CurrentHealth += 5;
             Debug.Log("체력 +5!");
