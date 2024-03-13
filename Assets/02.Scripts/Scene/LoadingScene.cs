5f644b7a156d3573b07d9bb73d12d6b7943ca729
MirDragon40 <mirjeong6463@gmail.com>
2024-03-13 16:52:57 +0900

    [정수빈] 그냥 커밋

--- Assets/02.Scripts/Scene/LoadingScene.cs
+++ Assets/02.Scripts/Scene/LoadingScene.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static LobbyScene;
 using UnityEngine.SceneManagement;
 
 
 
 public class LoadingScene : MonoBehaviour
 {
-
+    
 
     void Start()
     {
-        StartCoroutine(WaitAndLoadNextScene(5)); // 5초 대기 후 다음 씬 로드
+        StartCoroutine(WaitAndLoadNextScene(3)); // 5초 대기 후 다음 씬 로드
     }
 
     IEnumerator WaitAndLoadNextScene(float waitTime)
     {
-        yield return new WaitForSeconds(waitTime); // 파라미터로 받은 시간(초)만큼 대기
+
+        yield return new WaitForSecondsRealtime(waitTime); // 파라미터로 받은 시간(초)만큼 대기
 
         // 다음 씬 로드
         // 'NextScene'을 로드하고자 하는 씬의 이름 또는 인덱스로 교체하세요.
-        SceneManager.LoadScene("NextScene");
+        SceneManager.LoadScene("SuBin Academy");
     }
 
 }
