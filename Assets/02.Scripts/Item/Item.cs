b3b69ca6c072043c1be85da1b0c3b161371c8d45
SeungYeonKo <createNewScene479@gmail.com>
2024-03-13 10:37:35 +0900

    [고승연] 이펙트추가

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -32,10 +32,14 @@ public class Item
 
         if (ItemType == ItemType.Health)
         {
+            
             // Health아이템 사용시 플레이어 체력 +5
             ThirdPersonController ThirdPersonController = GameObject.FindWithTag("Player").GetComponent<ThirdPersonController>();
-            ThirdPersonController.CurrentHealth += 5;
-            Debug.Log("체력 +5!");
+            if(ThirdPersonController.CurrentHealth <= ThirdPersonController.MaxHealth)
+            {
+                ThirdPersonController.CurrentHealth += 5;
+                Debug.Log("체력 +5!");
+            }
         }
         return true;
     }
--- Assets/02.Scripts/Player/PlayerItemUseAbility.cs
+++ Assets/02.Scripts/Player/PlayerItemUseAbility.cs
@@ -8,6 +8,7 @@ public class PlayerItemUseAbility : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            
             // 체력 아이템 사용
             bool result = ItemManager.Instance.TryUseItem(ItemType.Health);
             Debug.Log("체력 아이템을 사용");
