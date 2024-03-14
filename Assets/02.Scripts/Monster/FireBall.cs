5353d754961e5fc6ec61ad3aa2f43fb1b949cbdc
SeungYeonKo <createNewScene479@gmail.com>
2024-03-14 17:30:23 +0900

    [고승연] 원거리 공격 몬스터 구현(이펙트, fireball이 player에 트리거했을 때 데미지 닳게는 못함)

--- Assets/02.Scripts/Monster/FireBall.cs
+++ Assets/02.Scripts/Monster/FireBall.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBall : MonoBehaviour
-{
+{ 
     public float FireBallSpeed = 2f;
     private Rigidbody _rigidbody;
 
-    private void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.velocity = transform.forward * FireBallSpeed;
     }
 
-  
+    public void Shoot(Vector3 targertPosition)
+    {
+        // 1.  방향을 구한다.
+        Vector3 dir = targertPosition - transform.position ;
+        dir.Normalize();
+        _rigidbody.AddForce(dir * FireBallSpeed);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
