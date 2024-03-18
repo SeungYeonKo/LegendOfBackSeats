7d996429a7580a487b1d97c80224cb68affd86dc
SeungYeonKo <createNewScene479@gmail.com>
2024-03-18 16:42:29 +0900

    [고승연] 텍스트프리팹전부수정 + 마법화살 피하세요 텍스트 프리팹 추가

--- Assets/02.Scripts/Trap/TrapStartArea.cs
+++ Assets/02.Scripts/Trap/TrapStartArea.cs
@@ -1,21 +1,38 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TrapStartArea : MonoBehaviour
 {
-        private void OnTriggerEnter(Collider other)
+    public TextMeshProUGUI AvoidArrowTextUI;
+
+    private void Start()
+    {
+        AvoidArrowTextUI.text = string.Empty;
+    }
+
+    private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
+                StartCoroutine(ShowAvoidArrowMessage());
                 Debug.Log("트랩시작지점 밟음");
-            // 씬 내의 모든 ArrowSpawner 인스턴스를 찾는 부분
-            ArrowSpawner[] arrowSpawners = FindObjectsOfType<ArrowSpawner>();
-            foreach (ArrowSpawner spawner in arrowSpawners)
-            {
-                spawner.StartSpawning(); // 각 ArrowSpawner에 대해 화살 발사를 시작
-            }
+                // 씬 내의 모든 ArrowSpawner 인스턴스를 찾는 부분
+                ArrowSpawner[] arrowSpawners = FindObjectsOfType<ArrowSpawner>();
+                foreach (ArrowSpawner spawner in arrowSpawners)
+                {
+                    spawner.StartSpawning(); // 각 ArrowSpawner에 대해 화살 발사를 시작
+                }
            }
         }
+
+    IEnumerator ShowAvoidArrowMessage()
+    {
+        AvoidArrowTextUI.gameObject.SetActive(true);
+        AvoidArrowTextUI.text = "날아오는 마법 화살을 피하세요 !";
+        yield return new WaitForSeconds(2f);
+        AvoidArrowTextUI.gameObject.SetActive(false);
+         }
     }
