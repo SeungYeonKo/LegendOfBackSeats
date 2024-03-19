faa3d789bf3a558205614759789688ee4a07dc05
MirDragon40 <mirjeong6463@gmail.com>
2024-03-19 17:05:49 +0900

    [정수빈] 박스 아이템 거의 완성

--- Assets/02.Scripts/Environment/Chest/ChestController.cs
+++ Assets/02.Scripts/Environment/Chest/ChestController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,18 +13,41 @@ public enum Items
 
 public class ChestController : MonoBehaviour
 {
+
+   
     public GameObject Player;
     public GameObject ChestInteractionUI;
+
+    [Header("상자 이펙트")]
+    public GameObject ChestItemEffect1;
+    public GameObject ChestItemEffect2;
+    public GameObject ChestItemEffect3;
+
+    [Header("Get Chest Text UI")]
+    public TextMeshProUGUI GetChest_ArrowItemUI;
+    public TextMeshProUGUI GetChest_HealthItemUI;
+    public TextMeshProUGUI GetChest_KeyItemUI;
+    
     public Animator _animator;
     public float ChestOpenDistance = 5f;
 
+    
 
     private bool _isNear = false;
+
+    public Items item;
+
     void Start()
     {
         _animator = GetComponent<Animator>();
         _animator.SetBool("Opened", false);
+
+        ChestItemEffect1.SetActive(false);
+        ChestItemEffect2.SetActive(false);
+        ChestItemEffect3.SetActive(false);
     }
+
+
     void Update()
     {
         CheckPlayerDistance();
@@ -33,10 +57,11 @@ public class ChestController : MonoBehaviour
 
             OpenChest();
             _animator.SetBool("Opened", true);
+            StartCoroutine(ChestEffect_Coroutine());
 
         }
-
     }
+
 
     private void CheckPlayerDistance()
     {
@@ -59,11 +84,47 @@ public class ChestController : MonoBehaviour
         }
     }
 
+
     private void OpenChest()
     {
         Debug.Log("상자가 열렸다!");
         ChestInteractionUI.SetActive(false);
         _animator.SetTrigger("Open");
         _animator.SetBool("Opened", true);
+
+
+        if (item == Items.Health)
+        {
+            GetChest_HealthItemUI.gameObject.SetActive(true);
+        }
+        else if (item == Items.Arrow)
+        {
+            GetChest_ArrowItemUI.gameObject.SetActive(true);
+        }
+        else if (item == Items.Key)
+        {
+            GetChest_KeyItemUI.gameObject.SetActive(true);
+        }
+
+    }
+
+    private IEnumerator ChestEffect_Coroutine()
+    {
+        yield return new WaitForSeconds(0.7f);
+       ChestItemEffect1.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        ChestItemEffect2.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        ChestItemEffect3.SetActive(true);
+
+    }
+
+    private IEnumerator GetChestItemUi_Coroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        GetChest_HealthItemUI.gameObject.SetActive(false);
+        GetChest_ArrowItemUI.gameObject.SetActive(false);
+        GetChest_KeyItemUI.gameObject.SetActive(false);
+
     }
 }
