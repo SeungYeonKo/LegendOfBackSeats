42c53ee50b99c6b60de1c3df880bb064ed68a267
MirDragon40 <mirjeong6463@gmail.com>
2024-03-20 11:20:14 +0900

    [정수빈] 출구 수정중

--- Assets/02.Scripts/Environment/EndingDoor/EndingDoorController.cs
+++ Assets/02.Scripts/Environment/EndingDoor/EndingDoorController.cs
@@ -13,36 +13,56 @@ public class EndingDoorController : MonoBehaviour
 
     private bool _isNear = false;
 
+    
+
+
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _animator.SetBool("Opened", false);
 
     }
 
     void Update()
     {
-        
+        CheckPlayerDistance();
+
+        if (_isNear && Input.GetKeyDown(KeyCode.E) /* && ItemManager.Instance.ItemList[2].Count >= 1*/)
+        {
+
+            OpenEndingDoor();
+            _animator.SetBool("Opened", true);
+
+        }
+
     }
 
     private void CheckPlayerDistance()
     {
         if (Vector3.Distance(Player.transform.position, transform.position) < DoorOpenDistance)
         {
-            if (!_isNear && !_animator.GetBool("Opened")) // 플레이어가 근처에 처음 도달했을 때 한 번만 UI를 활성화
+            if (!_isNear && !_animator.GetBool("Opened") /* &&ItemManager.Instance.ItemList[2].Count >= 1*/) // 플레이어가 근처에 처음 도달했을 때 (열쇠가 있을 경우) 한 번만 UI를 활성화
             {
                 UseKeyInteractionUI.SetActive(true);
                 _isNear = true;
-
             }
+
         }
         else
         {
-            if (_isNear && !_animator.GetBool("Opened")) // 플레이어가 멀어졌을 때 한 번만 UI를 비활성화
+            if (_isNear && !_animator.GetBool("Opened")/* &&ItemManager.Instance.ItemList[2].Count >= 1*/) // 플레이어가 멀어졌을 때 한 번만 UI를 비활성화
             {
                 UseKeyInteractionUI.SetActive(false);
                 _isNear = false;
             }
         }
     }
+    private void OpenEndingDoor()
+    {
+        Debug.Log("문이 열렸다!");
+       UseKeyInteractionUI.SetActive(false);
+        _animator.SetTrigger("Open");
+        _animator.SetBool("Opened", true);
 
+    }
 }
