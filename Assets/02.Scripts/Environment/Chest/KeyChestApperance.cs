a0f3c79c481cf20d78cdf63b12d5ba62c297988e
sjoonkim705 <sjoonkim705@gmail.com>
2024-03-20 10:13:59 +0900

    [김성준] 상자, 문열리는 소리 자르기

---
+++ Assets/02.Scripts/Environment/Chest/KeyChestApperance.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyChestApperance : MonoBehaviour
+{
+    // public GameObject KeyChestPrefab;
+    public GameObject AppearanceEffect;
+    public List<GameObject> TargetEnemies;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
