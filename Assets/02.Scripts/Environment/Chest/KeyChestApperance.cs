84fdd03d957828c053672aae1c8bc0764d1f99d5
sjoonkim705 <sjoonkim705@gmail.com>
2024-03-20 11:20:58 +0900

    [김성준] 키상자 등장조건 (지정된 적들 처치) 이벤트 script 구현

--- Assets/02.Scripts/Environment/Chest/KeyChestApperance.cs
+++ Assets/02.Scripts/Environment/Chest/KeyChestApperance.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,51 @@ public class KeyChestApperance : MonoBehaviour
 {
     // public GameObject KeyChestPrefab;
     public GameObject AppearanceEffect;
+    private GameObject _chest;
+    public float AppearTime = 1.0f;
+    public float CloseUpTime = 3.0f;
     public List<GameObject> TargetEnemies;
+    private bool _hasTriggered = false;
+    private CinemachineVirtualCamera _closeUpCamera;
+
 
     void Start()
     {
-
+        _chest = transform.GetChild(0).gameObject;
+        _chest.gameObject.SetActive(false);
+        _closeUpCamera = transform.GetChild(1).gameObject.GetComponent<CinemachineVirtualCamera>();
     }
 
     void Update()
     {
+        // 모든 게임 오브젝트가 파괴되었는지 확인
+        bool allDestroyed = true;
+        foreach (GameObject monster in TargetEnemies)
+        {
+            if (monster != null)
+            {
+                allDestroyed = false;
+                break;
+            }
+        }
 
+        if (allDestroyed && !_hasTriggered)
+        {
+            StartCoroutine(ChestAppear_Coroutine());
+            _hasTriggered = true;
+        }
+    }
+    private IEnumerator ChestAppear_Coroutine()
+    {
+        _closeUpCamera.gameObject.SetActive(true);
+        yield return new WaitForSeconds(AppearTime/2);
+        //연기
+        GameObject effect = Instantiate(AppearanceEffect);
+        effect.transform.position = transform.position;
+        yield return new WaitForSeconds(AppearTime/2);
+        //상자 appear
+        _chest.gameObject.SetActive(true);
+        yield return new WaitForSeconds(CloseUpTime);
+        _closeUpCamera.gameObject.SetActive(false);
     }
 }
